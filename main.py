e7c0009c1678f4a7cc2385c050171febe94cd3c7
yopxdxd <155833982+yopxdxd@users.noreply.github.com>
2024-03-26 17:07:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,6 @@ twitch_miner.mine(
       Streamer("WikyamYT", settings=StreamerSettings(make_predictions=False  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART, percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=234   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
       Streamer("SirDanDeMan", settings=StreamerSettings(make_predictions=False  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART, percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=234   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
     ],                                  # Array of streamers (order = priority)
-    ],                                  # Array of streamers (order = priority)
     followers=False,                    # Automatic download the list of your followers
     followers_order=FollowersOrder.ASC  # Sort the followers list by follow date. ASC or DESC
 )
