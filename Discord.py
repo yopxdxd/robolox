9a2059bd8f1601b1eb009a8e29c00eb29f0684cf
yopxdxd <155833982+yopxdxd@users.noreply.github.com>
2025-11-11 06:10:44 -0500

    Add files via upload

---
+++ Discord.py
@@ -0,0 +1,24 @@
+from textwrap import dedent
+
+import requests
+
+from TwitchChannelPointsMiner.classes.Settings import Events
+
+
+class Discord(object):
+    __slots__ = ["webhook_api", "events"]
+
+    def __init__(self, webhook_api: str, events: list):
+        self.webhook_api = webhook_api
+        self.events = [str(e) for e in events]
+
+    def send(self, message: str, event: Events) -> None:
+        if str(event) in self.events:
+            requests.post(
+                url=self.webhook_api,
+                data={
+                    "content": dedent(message),
+                    "username": "TL",
+                    "avatar_url": "https://www.sponsoo.com/uploads/profile-images/logo/team-liquid-766a5958482d81bac6385ec44f745e8c.png",
+                },
+            )
