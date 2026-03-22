77853f52b89171bc9624be5bbf622c6fd67f8dc4
yopxdxd <155833982+yopxdxd@users.noreply.github.com>
2026-03-22 17:15:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ twitch_miner.mine(
       Streamer("eplefort", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("hammafv", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("bacon_gaming15", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
+      Streamer("kozywater", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("ewwsumit", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("delicioustf2", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
       Streamer("crxdaniiel", settings=StreamerSettings(follow_raid=True , claim_drops=True  , watch_streak=True)),
